fed2c1ae961a755d3b9b4d03a48a7d1e847f7234
Robin De Schepper <robin.deschepper93@gmail.com>
2019-11-21 23:15:39 +0100

    reroute install command to pip

--- glia/cli.py
+++ glia/cli.py
@@ -9,6 +9,14 @@ def glia_cli():
     # imported, and glia installs itself on first import.
     install_self_parser.set_defaults(func=lambda: None)
 
+    install_parser = subparsers.add_parser("install", description="Install a glia package")
+    install_parser.add_argument("command", action="store", help="pip install command")
+    install_parser.set_defaults(func=install_package)
+
     cl_args = parser.parse_args()
     if hasattr(cl_args, 'func'):
         cl_args.func(cl_args)
+
+def install_package(args):
+    import subprocess, sys
+    subprocess.call([sys.executable, "-m", "pip", "install", args.command])
