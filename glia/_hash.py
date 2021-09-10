480f9cc2999c754fd5f5971da9358fb2bbea16d6
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-10 15:26:17 +0200

    better to put this in assertion message

--- glia/_hash.py
+++ glia/_hash.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 
 def hash_update_from_file(filename, hash):
-    assert Path(filename).is_file()
+    msg = f"Trying to file-hash non file path '{filename}'"
+    assert Path(filename).is_file(), msg
     with open(str(filename), "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash.update(chunk)
@@ -15,8 +16,8 @@ def hash_file(filename):  # pragma: nocover
 
 
 def hash_update_from_dir(directory, hash):
-    print("HASHING DIR", directory)
-    assert Path(directory).is_dir()
+    msg = f"Trying to directory-hash non directory path '{directory}'"
+    assert Path(directory).is_dir(), msg
     for path in sorted(Path(directory).iterdir()):
         hash.update(path.name.encode())
         if path.is_file():
