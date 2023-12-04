d7ef37282e4531db251396923e6f593fbdf9574a
Robin De Schepper <robin.deschepper93@gmail.com>
2023-12-04 17:42:54 +0100

    allow the tuple form to be passed in

---
+++ tests/test_resolve.py
@@ -0,0 +1,27 @@
+import unittest
+
+from glia.assets import Mod, Package
+from glia.resolution import Resolver
+
+
+class ManagerMock:
+    def __init__(self, packages):
+        self.packages = packages
+
+
+class TestResolver(unittest.TestCase):
+    """
+    Check if packages can be discovered.
+    """
+
+    def test_resolve(self):
+        pkg = Package("test", __file__, False)
+        m = Mod(pkg, "hello", "test_v")
+        mname = m.mod_name
+        pkg.mods = [m]
+        resolver = Resolver(ManagerMock([pkg]))
+        self.assertEqual(mname, resolver.resolve("hello"))
+        self.assertEqual(mname, resolver.resolve("hello", pkg="test", variant="test_v"))
+        # Test the tuple forms
+        self.assertEqual(mname, resolver.resolve(("hello",)))
+        self.assertEqual(mname, resolver.resolve(("hello", "test_v", "test")))
